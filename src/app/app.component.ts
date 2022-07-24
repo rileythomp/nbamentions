ab35b80c94e5c6faca89abc803e5b356d072671c
Riley Thompson <rileythompson99@gmail.com>
2022-07-24 15:56:37 -0400

    team mentions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,28 @@ export class AppComponent {
 	readonly ApiUrl = 'http://localhost:5000';
 	readonly limit = 25;
 
-	players: any[] = [];
+	mentionList: any[] = [];
 	upperBound: number = 0;
 	duration: string = 'week';
+	chartType: string = 'player';
 
-	getPlayers<T>(url: string): Promise<T> {
+	ngOnInit() {
+		this.getTrending()
+	}
+
+	ngAfterViewChecked() {
+		let barFlexes = document.getElementsByClassName('bar-flex')
+		if (barFlexes.length != 9) {
+			return
+		}
+		let mentionsBar = document.getElementsByClassName('mentions-bar')
+		for (let i = 0; i < 9; i++) {
+			// width of bar - 50 (for the image) times ratio of mentions to mentions upper bound
+			(<HTMLElement>mentionsBar[i]).style.width = (( (<HTMLElement>barFlexes[i]).clientWidth - 55 ) * ( this.mentionList[i].mentions / this.upperBound )).toString() + 'px'
+		}
+	}
+
+	getMentions<T>(url: string): Promise<T> {
 		return fetch(url)
 		  .then(response => {
 				if (!response.ok) {
@@ -26,42 +43,31 @@ export class AppComponent {
 		  })
 	}
 
-	getTrendingPlayers() {
-		this.getPlayers<any>(`${this.ApiUrl}/api/v1/trending?limit=${this.limit}&duration=${this.duration}`)
+	getTrending() {
+		this.getMentions<any>(`${this.ApiUrl}/api/v1/mentions?limit=${this.limit}&duration=${this.duration}&mention_type=${this.chartType}`)
 		.then(players => {
-				this.players = players
+				this.mentionList = players
 				let max = 0
-				for (let player of this.players) {
-					if (player.mentions > max) {
-						max = player.mentions
+				for (let mention of this.mentionList) {
+					if (mention.mentions > max) {
+						max = mention.mentions
 					}
 				}
 				// rounds numbers to their second most significant digit (e.g. 1234 -> 1300, 146,899 -> 150,000)
-				let orderOfMagnitude = Math.pow(10, Math.floor(Math.log10(max)) - 1)
+				let orderOfMagnitude = Math.max(Math.pow(10, Math.floor(Math.log10(max)) - 1), 10)
 				this.upperBound = Math.ceil(max/orderOfMagnitude)*orderOfMagnitude
 		}).catch(err => {
 				console.log(err);
 		})
 	}
 
-	ngOnInit() {
-		this.getTrendingPlayers()
-	}
-
-	ngAfterViewChecked() {
-		let barFlexes = document.getElementsByClassName('bar-flex')
-		if (barFlexes.length != 9) {
-			return
-		}
-		let mentionsBar = document.getElementsByClassName('mentions-bar')
-		for (let i = 0; i < 9; i++) {
-			// width of bar - 50 (for the image) times ratio of mentions to mentions upper bound
-			(<HTMLElement>mentionsBar[i]).style.width = (( (<HTMLElement>barFlexes[i]).clientWidth - 55 ) * ( this.players[i].mentions / this.upperBound )).toString() + 'px'
-		}
+	updateDuration(event: any) {
+		this.duration = event.target.value
+		this.getTrending()
 	}
 
 	updateChart(event: any) {
-		this.duration = event.target.value
-		this.getTrendingPlayers()
+		this.chartType = event.target.value
+		this.getTrending()
 	}
 }
